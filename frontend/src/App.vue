63b92ec627e1c8d6116b4d2219c982d041bc217f
Coffee_sw <897884964@qq.com>
2024-10-22 22:39:23 +0800

    ✨ feat: shaden-ui init

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { Button } from '@/components/ui/button'
 import { Greet } from '../wailsjs/go/main/App'
 import { ref } from 'vue'
 const data = ref('')
@@ -9,15 +9,7 @@ Greet('hello').then(res => {
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Button>111</Button> 
   <div>{{ data }}</div>
 </template>
 
